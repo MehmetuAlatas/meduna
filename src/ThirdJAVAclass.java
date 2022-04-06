2d25c53c7b375f238714ceb44e440395d441db7e
Mehmet Alatas <mehmetualatas@gmail.com>
2022-04-06 13:21:51 +0300

    updated

--- src/ThirdJAVAclass.java
+++ src/ThirdJAVAclass.java
@@ -1,6 +1,26 @@
 public class ThirdJAVAclass {
-    //this will also have some java code
-    /*
-    I ll improve it.
-     */
-}
+         /*Write a return method that can find the frequency of characters
+    frequencyOfChars("AAABBCDD") =>A3B2C1D2 */
+        public static void main(String[] args) {
+            getfrequencyOfChars("AAABBCDD");
+        }
+
+        private static void getfrequencyOfChars(String str) {
+            String freq = "";
+
+            for (int i = 0; i < str.length(); i++) {
+
+                int counter = 0;
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        counter++;
+                    }
+                }
+                if(!freq.contains(String.valueOf(str.charAt(i)))){
+                    freq += str.charAt(i) + "" + counter;
+                }
+            }
+
+            System.out.println(freq);
+        }
+    }
